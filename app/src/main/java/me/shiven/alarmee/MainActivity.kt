7b1d2120c4df3ffcebffe8f90df33b4630a8a85c
Shiven Saini <shiven.career@proton.me>
2025-02-10 19:14:45 +0530

    Misc : minor commit

--- app/src/main/java/me/shiven/alarmee/MainActivity.kt
+++ app/src/main/java/me/shiven/alarmee/MainActivity.kt
@@ -6,42 +6,29 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
+import me.shiven.alarmee.ui.alarm.AlarmApp
+import me.shiven.alarmee.ui.permission.PermissionApp
 import me.shiven.alarmee.ui.theme.AlarmeeTheme
+import me.shiven.alarmee.ui.userinfo.UserInfoApp
+import me.shiven.alarmee.ui.welcome.WelcomeApp
 
+@ExperimentalMaterial3Api
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AlarmeeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AlarmApp()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AlarmeeTheme {
-        Greeting("Android")
     }
 }
